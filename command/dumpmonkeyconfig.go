4fdef6bc04420ee636a753203e7210c84e9976c7
Vikneshwara R B <vikneshwara.rb@coda.global>
2021-02-06 23:16:14 +0530

    change name of source-package - test commit

--- command/dumpmonkeyconfig.go
+++ command/dumpmonkeyconfig.go
@@ -17,7 +17,7 @@ package command
 import (
 	"fmt"
 
-	"github.com/Netflix/chaosmonkey/config"
+	"github.com/vikneshwara-r-b/chaosmonkey/config"
 )
 
 // DumpMonkeyConfig dumps the monkey-level config parameters to stdout
